jcfr/dockgit-rocket-filter

commit 56795cb163cd523ca9765a82b78734225c4d0233

Dockerfile: Consolidate steps

Dockerfile
@@ -3,17 +3,12 @@ MAINTAINER Jean-Christophe Fillion-Robin "jchris.fillionr@kitware.com"
 
 ENV DEBIAN_FRONTEND noninteractive
 
-WORKDIR /usr/share
-
 RUN apt-get update && apt-get -y install \
   mono-complete \
-  unzip && \
-  apt-get -y clean
-
-RUN apt-get update && apt-get -y install \
+  unzip \
   curl \
-  runit && \
-  apt-get -y clean
+  runit \
+  && apt-get -y clean
 
 RUN curl -LO https://github.com/xoofx/GitRocketFilter/releases/download/v1.1.1/git-rocket-filter-v1.1.1.zip && \
   unzip git-rocket-filter-v1.1.1.zip -d git-rocket-filter && \

commit 7c1b26ddeb1d469f45c29e1f8b64756c243b2c44

Dockerfile: Ensure git-rocker-filter.exe is found
This commit fixes a regression introduced in 56795cb

Dockerfile
@@ -10,6 +10,8 @@ RUN apt-get update && apt-get -y install \
   runit \
   && apt-get -y clean
 
+WORKDIR /usr/share
+
 RUN curl -LO https://github.com/xoofx/GitRocketFilter/releases/download/v1.1.1/git-rocket-filter-v1.1.1.zip && \
   unzip git-rocket-filter-v1.1.1.zip -d git-rocket-filter && \
   rm git-rocket-filter-v1.1.1.zip